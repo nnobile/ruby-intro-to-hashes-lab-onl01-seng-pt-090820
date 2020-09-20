363aa9d2243aa69b72b5062a771699e74aa95867
Nick Nobile <nickcnobile@gmail.com>
2020-09-20 22:10:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-monopoly = {railroads: {:pieces => 4, names: {}, rent_in_dollars: {}
-end
+monopoly = {railroads: {:pieces => 4, names: {}, rent_in_dollars: {}}
 
 def monopoly_with_third_tier
 monopoly = {railroads:
